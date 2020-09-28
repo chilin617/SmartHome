21a31c17fa97bc6f1911c560487c5ba75ed392ba
chilin617 <as0975960617@gmail.com>
2020-09-28 15:52:19 +0800

    增加兩顆警示燈以0.5秒交替閃爍，並增加峰鳴器發聲功能

--- ArduinoSmartHomw1/ArduinoSmartHomw1.ino
+++ ArduinoSmartHomw1/ArduinoSmartHomw1.ino
@@ -3,6 +3,7 @@ void setup()
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
+  pinMode(7, OUTPUT);
 }
 
 void loop() 
@@ -14,9 +15,11 @@ void loop()
   digitalWrite(12, LOW);
   digitalWrite(11, HIGH);
   digitalWrite(10, LOW);
+  tone(7, 1000, 500);
   delay(500);
   digitalWrite(12, LOW);
   digitalWrite(11, LOW);
   digitalWrite(10, HIGH);
+  tone(7, 800, 500);
   delay(500);
 }
